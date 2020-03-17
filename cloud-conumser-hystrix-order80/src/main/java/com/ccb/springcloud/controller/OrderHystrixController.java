5138387acd1949c79a2e1b9447a6c025cdaf1de5
2568808909 <2568808909@qq.com>
2020-03-17 10:15:15 +0800

    DefaultProperties 配置默认降级方法

--- cloud-conumser-hystrix-order80/src/main/java/com/ccb/springcloud/controller/OrderHystrixController.java
+++ cloud-conumser-hystrix-order80/src/main/java/com/ccb/springcloud/controller/OrderHystrixController.java
@@ -1,6 +1,9 @@
 package com.ccb.springcloud.controller;
 
 import com.ccb.springcloud.service.feign.PaymentHystrixFeign;
+import com.netflix.hystrix.contrib.javanica.annotation.DefaultProperties;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,6 +11,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/consumer/order")
+//可以在类头上配置一个全局默认的降级方法，这样注解上@HystrixCommand却没有指定fallbackMethod的方法降级时就会走默认的方法
+//这样就不用一个方法配置一个fallbackMethod导致代码膨胀
+@DefaultProperties(defaultFallback = "timeoutDefault",commandProperties = {
+        @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "1500")
+})
 public class OrderHystrixController {
 
     @Autowired
@@ -19,7 +27,25 @@ public class OrderHystrixController {
     }
 
     @GetMapping("/timeout")
+    @HystrixCommand //没有配置fallbackMethod属性，如果出现故障或超时走默认的fallback方法
     public String timeout() {
         return paymentHystrixFeign.timeout();
+    }
+
+    @GetMapping("/time")
+    @HystrixCommand(fallbackMethod = "timeoutFallback", commandProperties = {
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "1500") //配置断路器，如果执行时间>5s则启进行服务降级
+    })
+    public String time() {
+        return paymentHystrixFeign.timeout();
+    }
+
+    public String timeoutFallback(){
+        return "timeoutFallback";
+    }
+
+    //返回值还是要跟降级前的方法保持一致
+    public String timeoutDefault() {
+        return "80 fail";
     }
 }
--- cloud-payment-hystrix-provider8001/src/main/java/com/ccb/springcloud/service/impl/PaymentHystrixServiceImpl.java
+++ cloud-payment-hystrix-provider8001/src/main/java/com/ccb/springcloud/service/impl/PaymentHystrixServiceImpl.java
@@ -14,10 +14,10 @@ public class PaymentHystrixServiceImpl implements PaymentHystrixService {
 
     @Override
     @HystrixCommand(fallbackMethod = "timeoutFallback", commandProperties = {
-            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "3000") //配置断路器，如果执行时间>5s则启进行服务降级
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "5000") //配置断路器，如果执行时间>5s则启进行服务降级
     })
     public String timeout() {
-        int time = 5000;
+        int time = 3000;
         try {
             Thread.sleep(time);
         } catch (InterruptedException e) {
@@ -30,6 +30,7 @@ public class PaymentHystrixServiceImpl implements PaymentHystrixService {
      * 无论是方法执行超时还是抛出异常到外部，都会执行这个指定的回调函数，进行服务降级
      * 值得注意的是，Hystrix调用Fallback函数时，使用并不是服务器本来的线程池，而是自己维护了一个，
      * 这样做能更好的使Fallback与执行主业务的线程隔离
+     *
      * @return
      */
     public String timeoutFallback() {
