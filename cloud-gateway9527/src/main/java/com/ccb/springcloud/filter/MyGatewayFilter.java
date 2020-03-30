16adb6b8c3d64ea3f143e1253559e6cad4615ecf
2568808909 <2568808909@qq.com>
2020-03-30 20:12:54 +0800

    自定义filter

---
+++ cloud-gateway9527/src/main/java/com/ccb/springcloud/filter/MyGatewayFilter.java
@@ -0,0 +1,31 @@
+package com.ccb.springcloud.filter;
+
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Component
+public class MyGatewayFilter implements GlobalFilter, Ordered {
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        String uname = exchange.getRequest().getQueryParams().getFirst("uname");
+        if (uname == null) { //如果不存在uname这个参数则不能进行路由
+            ServerHttpResponse response = exchange.getResponse();
+            response.setStatusCode(HttpStatus.NOT_ACCEPTABLE);
+            return response.setComplete();
+        }
+        return chain.filter(exchange);
+    }
+
+    //这个也可以实现Ordered而是通过加上@Order注解实现
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+}
