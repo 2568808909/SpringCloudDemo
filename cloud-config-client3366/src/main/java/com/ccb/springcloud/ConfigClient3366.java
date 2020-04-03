77da90e76a801f927903642d13459bf1ed98d335
2568808909 <2568808909@qq.com>
2020-04-03 20:15:50 +0800

    引入bus总线，方便动态刷新配置

--- cloud-config-client3366/src/main/java/com/ccb/springcloud/ConfigClient3366.java
+++ cloud-config-client3366/src/main/java/com/ccb/springcloud/ConfigClient3366.java
@@ -6,8 +6,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class ConfigClient3355 {
+public class ConfigClient3366 {
     public static void main(String[] args) {
-        SpringApplication.run(ConfigClient3355.class, args);
+        SpringApplication.run(ConfigClient3366.class, args);
     }
 }
