8723544222b891307876bf40e1d57ef611c12196
2568808909 <2568808909@qq.com>
2020-04-06 21:46:41 +0800

    sentinel

---
+++ cloudalibaba-sentinel-service8401/src/main/java/com/ccb/springcloud/controller/FlowLimitController.java
@@ -0,0 +1,20 @@
+package com.ccb.springcloud.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/flow")
+public class FlowLimitController {
+
+    @GetMapping("/testA")
+    public String testA() {
+        return "---------------------testA";
+    }
+
+    @GetMapping("/testB")
+    public String testB() {
+        return "---------------------testB";
+    }
+}
